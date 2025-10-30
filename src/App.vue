85efbebfddddec780732153cad6d19fdc1b27831
CGOMEZCASARES <cristina.gomez-casares@inetum.com>
2025-10-30 11:14:53 +0100

    tailwindcss and build

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1>Mi primera app</h1>
+  <h1 class="text-3xl font-bold underline">Mi primera app</h1>
   <hr />
-  <MyCounter :value="2" />
+  <MyCounter :value="2" text="hola" />
   <MyCounterScript :value="3" />
   <MyCounterScript2 :value="4" />
 </template>
--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -5,8 +5,8 @@
     <h3>Square: {{ squareConuter }}</h3>
     <hr />
     <div>
-      <button @click="incrementCounter()">+1</button>
-      <button @click="decrementCounter">-1</button>
+      <button class="btn mr-3" @click="incrementCounter()">+1</button>
+      <button class="btn" @click="decrementCounter">-1</button>
     </div>
   </section>
 </template>
@@ -43,3 +43,16 @@ const decrementCounter = () => {
   counter.value--;
 };
 </script>
+<style scoped>
+/* @reference 'tailwindcss'; */
+/* @tailwind components;
+@layer components {
+  .btn {
+    @apply py-2 px-5 bg-violet-500 min-w-10 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75;
+  }
+} */
+
+.btn {
+  @apply py-2 px-5 bg-violet-500 min-w-10 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75;
+}
+</style>
